e133e5400a2eed5d9806cfec280a24889bb3cf39
Rohaam <91160029+Rohaam-ROSHAN@users.noreply.github.com>
2022-02-10 21:06:00 +0100

    simple rules added, comma, dash,phrase, example

--- calf/calf.py
+++ calf/calf.py
@@ -11,16 +11,50 @@ def tokenise(sentence: str, iob_option):
     :param sentence: The sentence to tokenise
     :return: a 2-tuple. The first one is for the token array, the second one is for the IOB tags
     """
+
+    if len(sentence) == 0 : sentence ="J'habite à Nancy, il y a une boulangerie, dites-moi où tu habites, qu'est-ce qu'il y a au cinéma?"
+
     # Take care of the terminal symbol
+    #"J'habite à Nancy, il y a une boulangerie, que faites-vous chez vous, dites-moi où vous habitez ?" 
     if sentence[-1] not in considered_letters:
         # If there's not a space before the terminal symbol, add one
         if sentence[-2] != " ":
             well_formed_sentence = sentence[0:-1] + " " + sentence[-1]
             sentence = well_formed_sentence
+    
+    # Take care of some phrases
+    phras = { "Qu'est-ce": "Qu_est_ce" , "qu'est-ce": "qu_est_ce",  'il y a':'il_y_a'} # 'il y a':'il_y_a', 'Il y a':'Il_y_a'
+    new_sentence = sentence
+    for pop, tech in phras.items() :   
+        while True:
+            ind = sentence.find(pop)
+            if ind >= 0 : 
+                new_sentence = sentence[:ind] + tech + sentence[ind+len(pop): ] 
+                sentence = new_sentence
+            else : break
+        # sentence = new_sentence
 
     # Split the sentence using a space as separator
     space_seg = sentence.split(" ")
 
+    # Take care of comma 
+    sentence_seg = []
+    for item in space_seg :
+        if len (item) >1 and item[-1] == ',' :
+            sentence_seg.append(item[:-1])
+            sentence_seg.append(',')
+        elif len(item) >0 :
+            sentence_seg.append(item)
+    
+    # Take care of dash between two words
+    temp_seg = []
+    for item in sentence_seg :
+        if '-' in item and len(item)>1 :
+            temp_seg.extend( item.split('-') )
+        else :
+            temp_seg.append(item)
+
+    space_seg = temp_seg # renaming because of not disturbing rest of the code
     quote_seg = []
     refined_quote_seg = []
 
@@ -53,24 +87,38 @@ def tokenise(sentence: str, iob_option):
         else:
             refined_quote_seg.append(Token(quote_seg[i]))
     if iob_option:
-        iob = []
+        iob =[]
+        iob_tokens = []
+        iob_token = []
+        i = -1 # next loop counter
         # Take care of the IOB tags
         for token in refined_quote_seg:
+            i += 1
+            if i > 0 : iob_tokens.append(iob_token)
+            iob_token =[]
             # Beginning of the token
             iob.append("B")
+            iob_token.append("B")
             # Token's inners
             for smt in range(len(token.representation) - 1):
                 iob.append("I")
+                iob_token.append("I")
             # Do not add an token's outer if there exists a quote
             if "'" in token.representation:
                 continue
             # Handle the end of the sentence
             elif token is refined_quote_seg[-1]:
                 continue
+            # Taking care of comma in IOB presentation. Do not add an token's outer if there exists a comma delimiter.
+            elif i<len(refined_quote_seg)-1 and ( ',' in refined_quote_seg[i+1].representation   ) : 
+                continue
             # Add a token outer
             else:
                 iob.append("O")
-        return refined_quote_seg, iob
+                iob_token.append("O")
+        iob_tokens.append(iob_token)
+
+        return refined_quote_seg, iob_tokens
     return refined_quote_seg, []
 
 
@@ -113,7 +161,12 @@ if __name__ == "__main__":
         input_sentence = args.sentence
         input_iob_option = args.iob
         tokenisation_output = tokenise(input_sentence,input_iob_option)
-        print(tokenisation_output)
+        
+        print(*tokenisation_output[0])
+        # print(tokenisation_output[1])
+        [print(*x, sep='', end=' ') for x in tokenisation_output[1]]
+        print()
+        print('(',input_sentence,')')
     elif args.command == "extract":
         input_indir = args.indir
         input_outdir = args.outdir

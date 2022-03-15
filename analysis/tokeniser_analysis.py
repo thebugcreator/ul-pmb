ffa324de7fc637bbce9b0b7478d6a3751d4c55e7
thebugcreator <duyngovan1404@gmail.com>
2022-03-15 21:59:11 +0100

    Token representation modified

--- analysis/tokeniser_analysis.py
+++ analysis/tokeniser_analysis.py
@@ -1,9 +1,6 @@
-from nltk import ne_chunk, pos_tag
-from nltk.tokenize import word_tokenize, RegexpTokenizer, WordPunctTokenizer
-from nltk.metrics import ConfusionMatrix
-
-import spacy
 import pandas as pd
+import spacy
+from nltk.tokenize import word_tokenize, WordPunctTokenizer
 
 # Loading tsv into data frame
 df = pd.read_csv("../pmb_french_163.tsv", sep="\t")
@@ -16,6 +13,7 @@ for gt in gold_tokenisation:
 
 # Get the string literals for tokenisation
 gold_literals = df["French translation"]
+gold_ids = df["ID"]
 
 
 def get_nltk_tokenisation(string_literals: list) -> pd.DataFrame:
@@ -26,16 +24,16 @@ def get_nltk_tokenisation(string_literals: list) -> pd.DataFrame:
     """
     # Get nltk tokenisation
     # to evaluate the two different tokenisers provided by default by nltk
-    nltk_wt_tokenisation = [word_tokenize(item, language="french") for item in string_literals]
-    nltk_wp_tokenisation = [WordPunctTokenizer().tokenize(item) for item in string_literals]
+    nltk_wt_tokenisation = [" ".join(word_tokenize(item, language="french")) for item in string_literals]
+    nltk_wp_tokenisation = [" ".join(WordPunctTokenizer().tokenize(item)) for item in string_literals]
 
     # Export NLTK tokenisation to tsv to compare
     df_nltk = pd.DataFrame()
+    df_nltk["ID"] = gold_ids
     df_nltk["literal"] = string_literals
     df_nltk["wt"] = nltk_wt_tokenisation
     df_nltk["wp"] = nltk_wp_tokenisation
-    are_similar = [row["wt"] == row["wp"] for index, row in df_nltk.iterrows()]
-    df_nltk["are_similar"] = are_similar
+    df_nltk["are_similar"] = [int(row["wt"] == row["wp"]) for index, row in df_nltk.iterrows()]#
     return df_nltk
 
 
@@ -45,19 +43,21 @@ def get_spacy_tokenisation(string_literals: list) -> pd.DataFrame:
     pipeline_md = spacy.load("fr_core_news_md")
     tokenisation_sm = []
     tokenisation_md = []
+    are_similar = []
     # Get every sentence from gold literals into the pipeline
     for item in string_literals:
         doc_sm = pipeline_sm(item)
         doc_md = pipeline_md(item)
-        token_sm = [token.text for token in doc_sm]
-        token_md = [token.text for token in doc_md]
+        token_sm = " ".join([token.text for token in doc_sm])
+        token_md = " ".join([token.text for token in doc_md])
+        are_similar.append(int(token_md == token_sm))
         tokenisation_sm.append(token_sm)
         tokenisation_md.append(token_md)
     df_spacy = pd.DataFrame()
+    df_spacy["ID"] = gold_ids
     df_spacy["literal"] = string_literals
     df_spacy["sm"] = tokenisation_sm
     df_spacy["md"] = tokenisation_md
-    are_similar = [row["sm"] == row["md"] for index, row in df_spacy.iterrows()]
     df_spacy["are_similar"] = are_similar
     return df_spacy
 
@@ -66,6 +66,26 @@ def get_calf_tokenisation(string_literals: list) -> pd.DataFrame:
     pass
 
 
+def convert_to_iob(tokens):
+    """
+    To get the IOB tags of a list of tokens
+    :param tokens: A list of tokens
+    :return: IOB tags
+    """
+    iobs = []
+    for token in tokens:
+        if token:
+            iob = ["I"] * len(token)
+            iob[0] = "B"
+        else:
+            continue
+        if token != tokens[-1]:
+            iob.append("O")
+        iobs.append("".join(iob))
+    return "".join(iobs)
+
+
 # Export the results to tsv files
-get_nltk_tokenisation(gold_literals).to_csv("nltk_tokenisation.tsv", index=False, encoding="utf8", sep="\t")
-get_spacy_tokenisation(gold_literals).to_csv("spacy_tokenisation.tsv", index=False, encoding="utf8", sep="\t")
+# get_spacy_tokenisation(gold_literals).to_csv("spacy_tokenisation.tsv", index=False, encoding="utf8", sep="\t")
+# get_nltk_tokenisation(gold_literals).to_csv("nltk_tokenisation.tsv", index=False, encoding="utf8", sep="\t")
+
